91da79d80142aa2be1697d0a7997d1da933ee313
sivakumardev25 <sivakumarr2000@gmail.com>
2026-05-06 23:35:34 +0530

    Moved all the codes inside fontend file

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.qrtrack.app',
-  appName: 'qrtrackapp',
-  webDir: 'build'
-};
-
-export default config;
